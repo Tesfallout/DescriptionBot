c98ea415bf26acfe790b16ad99398d1c48377335
Tesfallout <tesfallout@gmail.com>
2024-09-02 16:39:44 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,10 +2,9 @@ import csv
 
 title = []      #0
 aired = []      #1
-airing = []     #2
-watched = []    #3
-comment = []    #4
-url = []        #5
+watched = []    #2
+tagline = []    #3
+url = []        #4
 
 temp = -1
 
@@ -13,23 +12,15 @@ with open('anime_data.csv', 'r', newline='') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for row in csv_reader:
-        title.append((str(row).split(',')[-7])[2:-1].replace("Ã—","x"))#title
-        aired.append(str(row).split(',')[-6][2:-1])     #aired
-        airing.append(str(row).split(',')[-5][2:-1])    #airing
-        watched.append(str(row).split(',')[-4][2:-1])   #watched
-        comment.append(str(row).split(',')[-3][2:-1])   #comment
-        url.append(str(row).split(',')[-2][2:-1])       #url
+        title.append(str(row).split(',')[0][2:-1])      #title
+        aired.append(str(row).split(',')[1][2:-1])      #aired
+        watched.append(str(row).split(',')[2][2:-1])    #watched
+        tagline.append(str(row).split(',')[3][2:-1])    #tagline
+        url.append(str(row).split(',')[4][2:-1])        #url
 
     for x in range(len(title)):
-        
-        if watched[x] != "D":
-            if watched[x].isdigit() and not "-E" in airing[x]:
-                print("\n" + title[x] + ": " + comment[x])
+        if watched[x] != "'D'":
+            if watched[x].isdigit() :
+                print("\n" + title[x] + ": " + tagline[x])
                 for i in range(int(aired[x])-int(watched[x])):
-                    print("<https://aniwave.to" + url[x] + "/ep-" + str(i+1+int(watched[x])) + ">")
-
-            elif "-E" in airing[x]: #show is not on the ongoing page of aniwave anymore
-                print("\n" + title[x] + ": " + comment[x])
-                temp = int(airing[x].split("-")[0])
-                for i in range(temp-int(watched[x])):
-                    print("<https://aniwave.to" + url[x] + "/ep-" + str(i+1+int(watched[x])) + ">")
+                    print("<https://ww8.gogoanimes.org/watch/" + url[x] + "-episode-" + str(i+1+int(watched[x])) + ">")
